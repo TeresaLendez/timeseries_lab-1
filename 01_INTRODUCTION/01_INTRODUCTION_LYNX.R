d4a00e8995f498ffbbaae5e10b8c1891a0f0053d
Ignaciovf <50411851+Ignaciovf@users.noreply.github.com>
2021-03-11 17:18:39 +0100

    Se agrega el código y la presentación de la sesión introductoria

---
+++ 01_INTRODUCTION/01_INTRODUCTION_LYNX.R
@@ -0,0 +1,21 @@
+#Vamos a hacer un ejemplo muy simple con los datos de la librería Forecast en R
+#Cargamos la librería
+library(forecast)
+
+#vamos a visualizar la serie
+plot(lynx)
+
+#para ajustar a un ARIMA vamos a llamar al Auto Arima
+forecast::auto.arima(lynx)
+
+#viendo lo que un AR2 MA2 nos daría el mejor resultado automático, vamos a probar hacer un forecast
+#creamos el objeto fc con nuestro modelo
+mod<-Arima(lynx, order = c(2,0,2))
+
+#vamos a ver cómo se ha ajustado
+plot(mod$x, col='black')
+lines(fitted(mod),col='red')
+
+#Vamos a visualizar el forecast
+prediction=forecast(fc, h=10)
+plot(prediction)
